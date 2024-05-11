90b11942b1196a58e59a13a3e5a42c814e0111eb
Özgür Yalçın <ozguryalcin@outlook.com>
2024-05-11 23:57:07 +0300

    Update Akbankpos.cs

--- Akbankpos.cs
+++ Akbankpos.cs
@@ -1,14 +1,29 @@
-using System.Reflection;
-using System.Security.Cryptography;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Xml;
-using System.Xml.Serialization;
-
 namespace Akbankpos {
     public enum MODE {
         Test,
         Prod
+    }
+    public class Akbankpos {
+        public string Mode { get; set; }
+        public string Endpoint { get; set; }
+        public string MerchantId { get; set; }
+        public string TerminalId { get; set; }
+        public string SecretKey { get; set; }
+        public void SetMerchantId(string merchantid) {
+            MerchantId = merchantid;
+        }
+        public void SetTerminalId(string terminalid) {
+            TerminalId = terminalid;
+        }
+        public void SetSecretKey(string secretkey) {
+            SecretKey = secretkey;
+        }
+        public Akbankpos(MODE mode) {
+            Endpoint = mode switch {
+                MODE.Test => "https://apipre.akbank.com",
+                MODE.Prod => "https://api.akbank.com",
+                _ => null
+            };
+        }
     }
 }
